e8040141b232e6dcea425f744f96877f76f3a297
IENGROUND <my@ien.zone>
2026-07-14 23:33:56 +0900

    ✨ feat(core): 코어 라이브러리 기능 개선 및 문서화 추가
    - 커스텀 연속성 구현을 위한 핵심 클래스와 확장 함수 추가
    - GitHub Actions 워크플로우 및 이슈 템플릿 구성 파일 추가
    - 빌드 설정과 의존성 관리를 위한 Gradle 파일 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,58 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    id("com.vanniktech.maven.publish") version "0.36.0" apply false
+    alias(libs.plugins.android.kotlin.multiplatform.library) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.compose.multiplatform) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.vanniktech.mavenPublish) apply false
+}
+
+val isReleaseVersion = !version.toString().endsWith("SNAPSHOT")
+
+subprojects {
+    plugins.withId("com.vanniktech.maven.publish") {
+        configure<com.vanniktech.maven.publish.MavenPublishBaseExtension> {
+            publishToMavenCentral(automaticRelease = true)
+
+            // Artifact ID만 각 프로젝트의 이름으로 자동 설정
+            group = "zone.ien.utils"
+            version = libs.versions.lib.version.name.get()
+            println("${group} ${project.name} ${version}")
+
+            coordinates(group.toString(), project.name, version.toString())
+
+            pom {
+                name = project.name
+                description = "Compose Multiplatform smooth corners"
+                inceptionYear = "2026"
+                url = "https://github.com/ienground/Capsule"
+                licenses {
+                    license {
+                        name = "Apache-2.0"
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "ienground"
+                        name = "Ericano Rhee"
+                        url = "my@ien.zone"
+                    }
+                }
+                scm {
+                    url = "https://github.com/ienground/Capsule.git"
+                    connection = "scm:git:https://github.com/ienground/Capsule.git"
+                    developerConnection = "scm:git:https://github.com/ienground/Capsule.git"
+                }
+            }
+
+            val isPublishingToMavenLocal = gradle.startParameter.taskNames.any { it.contains("publishToMavenLocal", ignoreCase = true) }
+            val isSnapshot = version.toString().endsWith("SNAPSHOT")
+
+            if (!isSnapshot && !isPublishingToMavenLocal) {
+                signAllPublications()
+            }
+        }
+    }
 }
--- capsule/src/main/java/com/kyant/capsule/MathUtils.kt
+++
@@ -1,6 +0,0 @@
-package com.kyant.capsule
-
-@Suppress("NOTHING_TO_INLINE")
-internal inline fun lerp(start: Double, stop: Double, fraction: Double): Double {
-    return start + (stop - start) * fraction
-}
